0d9499df9b9105622504590d2e92246918ce8ceb
Soichiro Kashima <soichiro.kashima@gmail.com>
2018-12-01 22:23:38 +0900

    Add test

--- test/index.js
+++ test/index.js
@@ -1,5 +1,7 @@
 var expect = require('chai').expect;
 var fs = require('fs');
+var path = require('path');
+var mkdirp = require('mkdirp');
 var temp = require('temp').track();
 var archiver = require('archiver');
 archiver.registerFormat('zip-encryptable', require('../'));
@@ -19,7 +21,7 @@ describe('archive', function() {
       // Local header
       expect(content.slice(0, 4)).to.eql(Buffer.from([0x50, 0x4b, 0x03, 0x04]));
       // File name length
-      expect(content.slice(0x1a, 0x1a + 4).readUInt32LE()).to.equal(0x08);
+      expect(content.slice(0x1a, 0x1a + 4).readUInt32LE()).to.equal('test.txt'.length);
       // File name
       expect(content.slice(0x1e, 0x1e + 8).toString('ascii')).to.eql('test.txt');
       // Data descripter
@@ -39,6 +41,58 @@ describe('archive', function() {
     archive.pipe(output);
     archive.append(Buffer.from('Hello World'), { name: 'test.txt' });
     archive.append(Buffer.from('Good Bye'), { name: 'test2.txt' });
+    archive.finalize();
+  });
+
+  it('archives file by filename', function(done) {
+    var tempDir = temp.mkdirSync('out');
+    mkdirp.sync(path.join(tempDir, 'out'));
+    var cwd = process.cwd();
+    var ws = fs.createWriteStream(tempDir + '/out/loremipsum.txt');
+    var fileContent = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.\n';
+    ws.write(fileContent);
+    ws.end();
+
+    var output = fs.createWriteStream(tempDir + '/example.zip');
+    output.on('close', function() {
+      process.chdir(cwd);
+      var content = fs.readFileSync(tempDir + '/example.zip');
+      // Local header
+      expect(content.slice(0, 4)).to.eql(Buffer.from([0x50, 0x4b, 0x03, 0x04]));
+      // File name length
+      var entryName = 'out/loremipsum.txt';
+      // File name
+      expect(content.slice(0x1e, 0x1e + entryName.length).toString('ascii')).to.eql(entryName);
+      done();
+    });
+    var archive = archiver('zip-encryptable', {
+      level: 9,
+      forceLocalTime: true,
+      password: 'test'
+    });
+    archive.pipe(output);
+    process.chdir(temp.dir);
+    archive.append('out/loremipsum.txt', { name: 'out/loremipsum.txt' });
+    archive.finalize();
+  });
+
+  it('archives files without compression', function(done) {
+    var tempDir = temp.mkdirSync('out');
+    var output = fs.createWriteStream(tempDir + '/example.zip');
+    output.on('close', function() {
+      var content = fs.readFileSync(tempDir + '/example.zip');
+      expect(content.slice(0, 4)).to.eql(Buffer.from([0x50, 0x4b, 0x03, 0x04]));
+      done();
+    });
+
+    var archive = archiver('zip-encryptable', {
+        zlib: { level: 0 },
+        forceLocalTime: true,
+        comment: 'test comment',
+        password: 'test'
+    });
+    archive.pipe(output);
+    archive.append(Buffer.from('Hello World'), { name: 'test.txt' });
     archive.finalize();
   });
 });
